98951404185c9a23346a7dd4df210b12c8edb242
Ankush5952 <ankush58008@gmail.com>
2024-09-01 14:07:28 +0530

    fixed everything finally v4

--- app/heart.py
+++ app/heart.py
@@ -38,7 +38,7 @@ class HeartDiseaseDiagnosis:
         # Load data and initialize variables
         self.heart = pd.read_csv('app/heart.csv')
         self.scaler = StandardScaler()
-        self.rf = RandomForestClassifier(criterion= 'entropy')
+        self.rf = RandomForestClassifier(criterion= 'entropy',max_depth=3)
         self.X_train, self.X_test, self.y_train, self.y_test = self.preprocess_data()
         self.y_pred = None
 
@@ -58,8 +58,8 @@ class HeartDiseaseDiagnosis:
         # Make predictions
         self.y_pred = self.rf.predict(X_test_scaled)
         # Print the accuracy score
-        # print('accuracy_score:', accuracy_score(self.y_pred, self.y_test))
-        print(self.y_pred)
+        print('accuracy_score:', accuracy_score(self.y_pred, self.y_test))
+        #print(self.y_pred)
 
     def getData(self):
         # Print the predictions
